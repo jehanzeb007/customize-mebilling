427f6c8de777491653dc5bdf83ea7c293cdcbd9d
abdullah <AbAbdullah786@githum.com>
2024-12-24 18:03:17 +0500

    working on section module

---
+++ resources/views/dashboard/sections/add-section.blade.php
@@ -0,0 +1,68 @@
+@extends('layout.app')
+@section('content')
+    <div class=" default-height">
+        <div class="container-fluid">
+            <div class="page-titles">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ route('admin.section.index') }}">Section</a></li>
+                    <li class="breadcrumb-item active"><a href="javascript:void(0)">Add Section</a></li>
+                </ol>
+            </div>
+            <!-- Row -->
+            <div class="row">
+                <div class="col-xl-12">
+
+
+
+                    <div class="row">
+                        <div class="col-xl-8">
+
+                            <form action="{{route('admin.section.store')}}" method="POST">
+                                @csrf
+
+                                <div class="form-group">
+                                    <label for="title">Title</label>
+                                    <input type="text" name="key" class="form-control"
+                                        value="{{ old('key', $contactSettings['latitude']->value ?? '') }}">
+                                </div>
+
+                                <!-- Short Description -->
+                                <div class="form-group">
+                                    <label for="short_description">Short Description</label>
+                                    <textarea name="value[short_description]" class="form-control" id="short-description">{{ old('description', $contactSettings['description']->value ?? '') }}</textarea>
+                                </div>
+
+                                <!-- long Description -->
+                                <div class="form-group">
+                                    <label for="long_description">Long Description</label>
+                                    <textarea name="value[long_description]" class="form-control" id="long-description">{{ old('description', $contactSettings['description']->value ?? '') }}</textarea>
+                                </div>
+
+                                <!-- Submit Button -->
+                                <div class="text-end mb-3">
+                                    <button type="submit" class="btn btn-primary">Save</button>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+@endsection
+
+@push('js')
+
+    {{-- editor --}}
+    <script src="https://cdn.tiny.cloud/1/z6kb5eytp65raef6tfmipfzb5i411ze1jwcxe05m5vccgz1b/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>
+        tinymce.init({
+            selector: '#short-description, #long-description',
+            plugins: 'lists table code image',
+            toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | table | image | code',
+            valid_elements: '*[*]',
+        })
+    </script>
+@endpush
---
+++ resources/views/dashboard/sections/index.blade.php
@@ -0,0 +1,122 @@
+@extends('layout.app')
+@section('content')
+    <div class=" default-height">
+        <div class="container-fluid">
+            <div class="page-titles">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">Section</a></li>
+                    <li class="breadcrumb-item active"><a href="javascript:void(0)">List</a></li>
+                </ol>
+            </div>
+            <!-- Row -->
+            <div class="row">
+                <div class="col-xl-12">
+                    {{-- <div class="filter cm-content-box box-primary">
+                        <div class="content-title">
+                            <div class="cpa">
+                                <i class="fa-solid fa-filter me-2"></i>Filter
+                            </div>
+                            <div class="tools">
+                                <a href="javascript:void(0);" class="expand SlideToolHeader"><i
+                                        class="fal fa-angle-down"></i></a>
+                            </div>
+                        </div>
+                        <div class="cm-content-body  form excerpt">
+                            <div class="card-body pb-3">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <input type="text" class="form-control mb-3" id="exampleFormControlInput1"
+                                            placeholder="Name">
+                                    </div>
+
+                                    <div class="col-4">
+                                        <input class="form-control  mb-3" type="text" id="datepicker">
+                                    </div>
+                                    <div class="col-4">
+                                        <button class="btn btn-primary" title="Click here to Search" type="button"><i
+                                                class="fa-solid fa-filter me-1"></i>Filter</button>
+                                        <button class="btn btn-danger light" title="Click here to remove filter"
+                                            type="button">Remove Filter</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div> --}}
+
+                    <div class="filter cm-content-box box-primary">
+                        <div class="content-title">
+                            <div class="cpa">
+                                <i class="fa-solid fa-file-lines me-1"></i>Section List
+                            </div>
+                            <div class="tools">
+                                <a href="javascript:void(0);" class="expand SlideToolHeader"><i
+                                        class="fal fa-angle-down"></i></a>
+                            </div>
+                        </div>
+                        <div class="cm-content-body form excerpt">
+                            <div class="card-body py-3">
+
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-condensed flip-content">
+                                        <thead>
+                                            <tr>
+                                                <th class="text-black">S.No</th>
+                                                <th class="text-black">Title</th>
+                                                <th class="text-black">Value</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {{-- @foreach ($currentSettings as $index => $setting)
+                                                <tr>
+                                                    <td>{{ $loop->iteration }}</td>
+                                                    <td>{{ ucwords(str_replace('_', ' ', $setting->key)) }}</td>
+                                                    <td>
+                                                        <!-- Check if the value is an image URL or path -->
+                                                        @if (preg_match('/\.(jpg|jpeg|png|gif)$/i', $setting->value))
+                                                            <!-- Display the image if value is a valid image path -->
+                                                            <img src="{{ asset('storage/' . $setting->value) }}"
+                                                                alt="Image" style="width: 100px; height: auto;">
+                                                        @else
+                                                            <!-- Otherwise, display the text value -->
+                                                            {{ $setting->value }}
+                                                        @endif
+                                                    </td>
+                                                </tr>
+                                            @endforeach --}}
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="text-end mb-3">
+                                    <a href="{{ route('admin.settings.settingpage') }}" class="btn btn-primary"
+                                        type="submit">
+                                        {{-- @if ($currentSettings->count() > 0)
+                                            Edit Settings
+                                        @else
+                                            Add Settings
+                                        @endif --}}
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+{{-- @push('js')
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+
+    <script>
+        $(function() {
+            $("#datepicker").datepicker({
+                autoclose: true,
+                todayHighlight: true
+            }).datepicker('update', new Date());
+
+        });
+    </script>
+@endpush --}}
